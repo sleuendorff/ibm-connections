1566df55e216e0e4761a7b71eaff9dc88da3071a
H.C.L. Barz <hcl@pokeshot.com>
2018-07-25 16:07:34 +0200

    values set incl. API key

--- mock_soyhook.js
+++ mock_soyhook.js
@@ -1,30 +1,30 @@
 
-        if (!window.SMZ) {window.SMZ = {}; }
-        SMZ.multidoc = {
-            settings: {
-                currentUserNativeLanguage: '{$currentUserLocaleCode}',
-                autotranslateEnabled: '{$translationsEnabled}',
-                autotranslateDefault: '{$translateDefault}',
-                rememberSettings: '{$rememberSettings}',
-                availableTargetLangsCSV: '{$availableTargetLangs}',
-                autoRedirect: '{$autoRedirect}',
-                searchTranslateDefault: '{$searchTranslateDefault}',
+if (!window.SMZ) {window.SMZ = {}; }
+SMZ.multidoc = {
+	settings: {
+		currentUserNativeLanguage: 'en',
+		autotranslateEnabled: 'true',
+		autotranslateDefault: 'false',
+		rememberSettings: 'true',
+		availableTargetLangsCSV: 'de,en,fr',
+		autoRedirect: 'true',
+		searchTranslateDefault: 'true',
 
-                provider: {
-                    name: '{$translationProvider}',
-                    url: '{$translationUrl}',
-                    key: '{$translationKey}',
-                    getTokenActionUrl : 'smz-get-accesstoken.jspa',
-                    maxSnippetLength:'{$maxSnippetLength}',
-                    about: '{i18nText('smz.multidoc.autotranslate.provider.about')} '
-                },
-                
-                translatedSelectors: {
-                    staticSelectors: [{$staticSelectors|noAutoescape}],
-                    dynamicSelectors: [{$dynamicSelectors|noAutoescape}]
-                },
-                
-                checkboxLabel: '{i18nText('smz.multidoc.autotranslate.translate.label')} ' + '{$currentUserLocaleCode}',                
-                checkboxLocation: ''
-            }
-        };
+		provider: {
+			name: 'GOOGLE',
+			url: 'https://translation.googleapis.com/language/translate/v2',
+			key: 'AIzaSyCW0mklASW5R-FLsAjh4nNGhwInHOM08dg',
+			getTokenActionUrl : 'smz-get-accesstoken.jspa',
+			maxSnippetLength:'null',
+			about: ' ABOUT AUTOTRANS '
+		},
+		
+		translatedSelectors: {
+			staticSelectors: [null],
+			dynamicSelectors: [null]
+		},
+		
+		checkboxLabel: 'Translate to:' + 'en',                
+		checkboxLocation: '.tabNavInner'
+	}
+};
